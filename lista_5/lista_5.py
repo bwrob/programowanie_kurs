70f51179893cef7e56e5eba58987ce4a625bc554
bwrob <bartosz.marcin.wroblewski@gmail.com>
2025-04-09 08:58:39 +0200

    lista 5 zad 3

--- lista_5/lista_5.py
+++ lista_5/lista_5.py
@@ -6,9 +6,18 @@ Calculate Levenshtein distance and guess the most similar string from a list.
 
 import time
 from collections import Counter
+from itertools import permutations
+from logging import INFO, Formatter, getLogger
 
 import numpy as np
-import rich
+from rich.logging import RichHandler
+
+log = getLogger(__name__)
+
+# Add a RichHandler to the logger
+handler = RichHandler(show_time=False)  # Disable timestamp in RichHandler
+handler.setFormatter(Formatter("%(message)s"))
+log.addHandler(handler)
 
 __FREQ = {
     "A": 0.099,
@@ -38,7 +47,7 @@ __FREQ = {
     "Y": 0.0376,
     "Z": 0.0653,
 }
-__MESSAGE = """CNJRYVTNJRYJWRQALZFGNYVQBZH
+__CESEAR_MESSAGE = """CNJRYVTNJRYJWRQALZFGNYVQBZH
 CNJRYANTBEMRNTNJRYANQBYR
 CNJRYFCBXBWALAVRJNQMVYAVXBZH
 TNJRYANWQMVXFMRJLZLFYNYFJNJBYR
@@ -46,6 +55,27 @@ PVNTYRCBYBJNYCBFJBVZCBXBWH
 GBCVRFGBMNWNPZVRQMLFGBYLFGBYXV
 TBAVYHPVRXNYJLJENPNYXBMVBYXV
 FGEMRYNYVGENOVYVXEMLPMNYQBMABWH"""
+
+__KEYWORDS = {
+    "kalafior",
+    "rower",
+    "krowa",
+    "pieczarka",
+    "prezydent",
+    "usa",
+    "pi",
+    "sigma",
+    "python",
+    "naleśniki",
+}
+
+__PARALLEL_MESSAGE = "uslppiapniepyrtswczehazdoyrkcnadvientjqlkjeogijpzxczx"
+
+__TEST_MESSAGE = "amaurtcheobwuksa"
+__TEST_KEYWORDS = {
+    "marchewka",
+    "autobus",
+}
 
 
 def koduj(napis: str, klucz: int = 1) -> str:
@@ -66,14 +96,14 @@ def test_koduj():
     assert koduj("A") == "B"
     assert koduj("Z") == "A"
     assert koduj("ALAMAKOTA", 12) == "MXMYMWAFM"
-    print("Wszystkie testy przeszły pomyślnie.")
+    log.debug("Wszystkie testy przeszły pomyślnie.")
 
 
 def test_dekoduj():
     assert dekoduj("B") == "A"
     assert dekoduj("A") == "Z"
     assert dekoduj("MXMYMWAFM", 12) == "ALAMAKOTA"
-    print("Wszystkie testy przeszły pomyślnie.")
+    log.debug("Wszystkie testy przeszły pomyślnie.")
 
 
 def porównaj(freq1, freq2):
@@ -108,12 +138,15 @@ def odległości_dla_kluczy(napis: str) -> dict[int, float]:
 
 
 def zad_1():
-    odleglosci_dict = odległości_dla_kluczy(__MESSAGE)
-    print("Najmniejsza odległość:")
+    test_koduj()
+    test_dekoduj()
+
+    odleglosci_dict = odległości_dla_kluczy(__CESEAR_MESSAGE)
+    log.debug("Najmniejsza odległość:")
     min_key = min(odleglosci_dict, key=odleglosci_dict.get)
-    print(f"Klucz {min_key}: {odleglosci_dict[min_key]}")
-    print("Zdekodowana wiadomość:")
-    print(dekoduj(__MESSAGE, min_key))
+    log.debug("Klucz %s: %s", min_key, odleglosci_dict[min_key])
+    log.info("Zdekodowana wiadomość:")
+    log.info(dekoduj(__CESEAR_MESSAGE, min_key))
 
 
 def levenshtein(napis1: str, napis2: str) -> int:
@@ -136,7 +169,8 @@ def levenshtein(napis1: str, napis2: str) -> int:
                     macierz[i][j - 1] + 1,  # Wstawienie
                     macierz[i - 1][j - 1] + 2,  # Zamiana
                 )
-
+    log.debug("Macierz:\n%s", np.array(macierz))
+    log.debug("Odległość Levenshteina: %s", macierz[len1][len2])
     return macierz[len(napis1)][len(napis2)]
 
 
@@ -156,6 +190,9 @@ def levenshtein_2(napis1: str, napis2: str) -> int:
             macierz[i, j] + koszt,  # Zamiana
         )
 
+    log.debug("Macierz:\n%s", macierz)
+    log.debug(f"Odległość Levenshteina: {macierz[len1, len2]}")
+
     return macierz[len1, len2]
 
 
@@ -163,59 +200,137 @@ def guess(napis: str, lista: list[str]) -> str:
     """Funkcja zgaduje, który z napisów jest najbardziej podobny do podanego."""
     odleglosci = [(levenshtein(napis, slowo), slowo) for slowo in lista]
     odleglosci.sort()
+    log.debug(f"Odległości: {odleglosci}")
+
     min_odleglosci = odleglosci[0][0]
     return [slowo for odleglosc, slowo in odleglosci if odleglosc == min_odleglosci]
 
 
-def zad_2() -> None:
+def zad_2(*, perf_test: int = False) -> None:
     """Funkcja testująca odległość Levenshteina."""
-    napis_1 = "Ala ma kota i wspierających rodziców." * 50
-    napis_2 = "Olek ma dom, samochód i bogatych rodziców." * 50
-    iterations = 100
+    napis_1 = "Ala ma kota i wspierających rodziców."
+    napis_2 = "Olek ma dom, samochód i bogatych rodziców."
 
-    # Measure performance of levenshtein
-    times_levenshtein = []
-    for _ in range(iterations):
-        start = time.perf_counter()
-        levenshtein(napis_1, napis_2)
-        end = time.perf_counter()
-        times_levenshtein.append(end - start)
+    if perf_test:
+        iterations = 100
+        level = log.level
+        log.setLevel(INFO)
+        # Measure performance of levenshtein
+        times_levenshtein = []
+        for _ in range(iterations):
+            start = time.perf_counter_ns()
+            levenshtein(napis_1, napis_2)
+            end = time.perf_counter_ns()
+            times_levenshtein.append(end - start)
 
-    # Measure performance of levenshtein_2
-    times_levenshtein_2 = []
-    for _ in range(iterations):
-        start = time.perf_counter()
-        levenshtein_2(napis_1, napis_2)
-        end = time.perf_counter()
-        times_levenshtein_2.append(end - start)
+        # Measure performance of levenshtein_2
+        times_levenshtein_2 = []
+        for _ in range(iterations):
+            start = time.perf_counter_ns()
+            levenshtein_2(napis_1, napis_2)
+            end = time.perf_counter_ns()
+            times_levenshtein_2.append(end - start)
+        log.setLevel(level)
+        log.info("\nPorównanie wydajności:")
+        log.info(
+            "Levenshtein - Średni czas: %ss, Minimalny czas: %ss",
+            np.mean(times_levenshtein),
+            np.min(times_levenshtein),
+        )
+        log.info(
+            "Levenshtein_2 - Średni czas: %ss, Minimalny czas: %ss",
+            np.mean(times_levenshtein_2),
+            np.min(times_levenshtein_2),
+        )
 
-    print(
-        f"Odległość Levenshteina między '{napis_1}' a '{napis_2}':\n"
-        f"{levenshtein(napis_1, napis_2)}\n"
-        f"{levenshtein_2(napis_1, napis_2)}",
+    log.info(
+        "Odległość Levenshteina między '%s' a '%s':\n%s\n%s",
+        napis_1,
+        napis_2,
+        levenshtein(napis_1, napis_2),
+        levenshtein_2(napis_1, napis_2),
     )
 
-    print("\nPorównanie wydajności:")
-    print(
-        f"Levenshtein - Średni czas: {np.mean(times_levenshtein)}s, "
-        f"Minimalny czas: {np.min(times_levenshtein)}s",
-    )
-    print(
-        f"Levenshtein_2 - Średni czas: {np.mean(times_levenshtein_2)}s, "
-        f"Minimalny czas: {np.min(times_levenshtein_2)}s",
-    )
-
-    print("\nZgadnij, który z napisów jest najbardziej podobny do 'Ala':")
+    log.info("\nZgadnij, który z napisów jest najbardziej podobny do 'Ala':")
     lista = ["Olek", "Ola", "Ala ma kota"]
-    print("Lista:", lista)
-    print("Najbardziej podobne napisy:")
+    log.info("Lista: %s", lista)
+    log.info("Najbardziej podobne napisy:")
     podobne_napisy = guess("Ala", lista)
-    rich.print(podobne_napisy)
+    log.info(podobne_napisy)
+
+
+def odplatanie(msg: str, slowa: set[str]) -> list[str]:
+    msg_counter = Counter(msg)
+    mozliwe_slowa = sorted(
+        (slowo for slowo in slowa if Counter(slowo) <= msg_counter),
+        key=lambda x: len(x),
+    )
+    log.info(
+        "Słowa po odfiltorwaniu %s %s\n%s",
+        len(slowa),
+        len(mozliwe_slowa),
+        mozliwe_slowa,
+    )
+
+    msg_list = list(msg)
+    log.info("Wiadomość %s : %s", len(msg_list), msg_list)
+
+    slowa_chars = {char for slowo in mozliwe_slowa for char in slowo}
+    log.debug("Litery: %s", slowa_chars)
+
+    msg_list = [litera for litera in msg_list if litera in slowa_chars]
+    log.info("Wiadomość po odfiltrowaniu %s : %s", len(msg_list), msg_list)
+
+    results = []
+    max_current = 0
+
+    for per in permutations(mozliwe_slowa):
+        per_list = msg_list.copy()
+        result = []
+
+        for slowo in per:
+            slowo_list = per_list.copy()
+            for litera in slowo:
+                if litera not in slowo_list:
+                    break
+                slowo_list[slowo_list.index(litera)] = None
+            else:
+                per_list = slowo_list
+                result.append(slowo)
+
+        log.debug("Znalezione slowa: %s", result)
+        log.debug("Pozostałe litery: %s", per_list)
+
+        log.debug(sum(len(slowo) for slowo in result))
+
+        assert sum(len(slowo) for slowo in result) == len(
+            [c for c in per_list if c is None],
+        )
+
+        if len(result) > max_current:
+            max_current = len(result)
+            log.info("Znaleziono nowy maks %s: %s", max_current, result)
+            log.info("Pozostałe litery: %s", per_list)
+
+        results.append(result)
+        result = []
+
+    max_slow = max(len(result) for result in results)
+    return {tuple(sorted(result)) for result in results if len(result) == max_slow}
+
+
+def zad_3():
+    znalezione = odplatanie(__PARALLEL_MESSAGE, slowa=__KEYWORDS)
+    log.info(
+        "znaleziono %s rozwiazań dlugości %s :\n%s",
+        len(znalezione),
+        len(list(znalezione)[0]) if znalezione else 0,
+        znalezione,
+    )
 
 
 if __name__ == "__main__":
-    test_koduj()
-    test_dekoduj()
-    zad_1()
-
-    zad_2()
+    log.setLevel(INFO)
+    # zad_1()
+    # zad_2()
+    zad_3()
