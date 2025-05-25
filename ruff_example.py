b7caec5af1ed7eb8220afab6e6d1f3ba6dd856c3
bwrob <bartosz.marcin.wroblewski@gmail.com>
2025-05-25 13:41:50 +0200

    ruff example

---
+++ ruff_example.py
@@ -0,0 +1,11 @@
+def return_list(lista):
+    suma = 0
+    for x in lista:
+        
+        suma += x
+    return lista
+def is_greater_than_one(x):
+    if x>1:  
+        return True
+    else:
+        return False
